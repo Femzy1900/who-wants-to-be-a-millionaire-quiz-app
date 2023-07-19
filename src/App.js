f4485c3ac6007508a15c30da82f6807ac0eeaaf7
unknown <adedokunfemi14@gmail.com>
2023-07-19 07:16:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import Main from "./components/main";
-import Navbar from "./components/Navbar";
-
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Main />
-      {/* <Tuts />
-      <State /> */}
+      <h1>Hello World</h1>
     </div>
   );
 }

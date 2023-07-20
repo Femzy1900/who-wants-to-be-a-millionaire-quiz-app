18a4532749b5595f799c93823f4a55845ea07cfb
unknown <adedokunfemi14@gmail.com>
2023-07-20 12:07:05 -0700

    Update

---
+++ src/components/trivial.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+
+
+function Trivial({data, setTimeOut, setQuestionNumber, questionNumber}) {
+
+    const [question, setQuestion] = useState(null);
+    const [selectedAnswer, setSelectedAnswer] = useState(null)
+    const [className, setClassName] = useState("answer");
+
+
+    useEffect(() => {
+        setQuestion(data[questionNumber - 1])
+    }, [data, questionNumber])
+
+    const handleClick = (a) => {
+        setSelectedAnswer(a)
+        setClassName("answer active");
+        delay(1000, () => {
+               setClassName(a.correct ? "answer correct" : "answer wrong");
+            }
+        );
+
+        delay(3000, () => {
+            if(a.correct) {
+                setQuestionNumber((prev) => prev + 1);
+                setSelectedAnswer(null)
+
+            } else {
+                setTimeOut(true)
+            }
+        })
+
+    
+    }
+    const delay = (duration, callback) => {
+        setTimeout(() => {
+            callback();
+        }, duration);
+    }
+  return (
+    <div className='trivia'>
+        <div className="questions">
+            {question?.question}
+        </div>
+        <div className="answers">
+            {question?.answers.map((a) => (
+                <div className={selectedAnswer === a ? className : "answer"} onClick={() => handleClick(a)}>
+                    {a.text}
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Trivial  
---
+++ src/data.js
@@ -0,0 +1,105 @@
+const quizData = [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      answers: [
+        { text: "London", correct: false },
+        { text: "Berlin", correct: false },
+        { text: "Paris", correct: true },
+        { text: "Rome", correct: false },
+      ],
+    },
+    {
+      id: 2,
+      question: "Which planet is known as the 'Red Planet'?",
+      answers: [
+        { text: "Venus", correct: false },
+        { text: "Mars", correct: true },
+        { text: "Jupiter", correct: false },
+        { text: "Saturn", correct: false },
+      ],
+    },
+    {
+      id: 3,
+      question: "What is the chemical symbol for water?",
+      answers: [
+        { text: "O2", correct: false },
+        { text: "H2O", correct: true },
+        { text: "CO2", correct: false },
+        { text: "N2", correct: false },
+      ],
+    },
+    {
+      id: 4,
+      question: "Which country won the FIFA World Cup in 2018?",
+      answers: [
+        { text: "Brazil", correct: false },
+        { text: "Germany", correct: false },
+        { text: "France", correct: true },
+        { text: "Spain", correct: false },
+      ],
+    },
+    {
+      id: 5,
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      answers: [
+        { text: "William Shakespeare", correct: true },
+        { text: "Jane Austen", correct: false },
+        { text: "Charles Dickens", correct: false },
+        { text: "Mark Twain", correct: false },
+      ],
+    },
+    {
+      id: 6,
+      question: "What is the largest mammal in the world?",
+      answers: [
+        { text: "Elephant", correct: false },
+        { text: "Giraffe", correct: false },
+        { text: "Blue Whale", correct: true },
+        { text: "Hippopotamus", correct: false },
+      ],
+    },
+    {
+      id: 7,
+      question: "Which famous scientist formulated the theory of relativity?",
+      answers: [
+        { text: "Isaac Newton", correct: false },
+        { text: "Albert Einstein", correct: true },
+        { text: "Galileo Galilei", correct: false },
+        { text: "Charles Darwin", correct: false },
+      ],
+    },
+    {
+      id: 8,
+      question: "What is the chemical symbol for gold?",
+      answers: [
+        { text: "Au", correct: true },
+        { text: "Ag", correct: false },
+        { text: "Fe", correct: false },
+        { text: "Cu", correct: false },
+      ],
+    },
+    {
+      id: 9,
+      question: "Who painted the Mona Lisa?",
+      answers: [
+        { text: "Pablo Picasso", correct: false },
+        { text: "Leonardo da Vinci", correct: true },
+        { text: "Vincent van Gogh", correct: false },
+        { text: "Salvador Dalí", correct: false },
+      ],
+    },
+    {
+      id: 10,
+      question: "Which continent is known as the 'Dark Continent'?",
+      answers: [
+        { text: "North America", correct: false },
+        { text: "Europe", correct: false },
+        { text: "Asia", correct: false },
+        { text: "Africa", correct: true },
+      ],
+    },
+  ];
+  
+  export default quizData;
+  
